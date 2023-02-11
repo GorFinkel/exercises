c1303a9a707e91a753d6d6ef7e22359ef2171a3c
GorFinkel <97066710+GorFinkel@users.noreply.github.com>
2023-02-12 01:37:47 +0200

    Create figLatin-ay.js

---
+++ codewars/figLatin-ay.js
@@ -0,0 +1,26 @@
+function pigIt(stra){
+  //Code here
+  let arrOfWords=stra.split(' ')
+  let pun=/[^\w\s\']/
+if(pun.test(stra)){
+    arrOfWords.pop()
+  }
+  for (let i=0; i<arrOfWords.length; i++){
+    let letter=arrOfWords[i][0]
+    arrOfWords[i]=arrOfWords[i].slice(1,arrOfWords[i].length)
+    arrOfWords[i]+=letter+'ay'
+}
+  return arrOfWords.join(' ')
+}
+
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+
+// \w is any digit, letter, or underscore.
+// \s is any whitespace.
+// [^\w\s\'] is anything that's not a digit, letter, whitespace, underscore or a single quote.
+// [^\w\s\']|_ is the same as #3 except with the underscores added back in.
